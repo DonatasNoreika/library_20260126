7f659eb89f19cff6c9f0acfdee3ab8c812d7c4a1
DonatasNoreika <donoras@gmail.com>
2026-01-28 08:56:26 +0200

    3: Patobulintas knygų kopijų sąrašas, forma, prie knygos rodomi jos egzemplioriai

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+class BookInstanceInLine(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'isbn', 'display_genre']
+    inlines = [BookInstanceInLine]
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'due_back', 'status']
+    list_filter = ['status', 'due_back', 'book']
+
+    fieldsets = [
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    ]
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
