f84b23af35e575bcbb2a8c0adb63438b7c9497df
DonatasNoreika <donoras@gmail.com>
2026-02-10 09:49:31 +0200

    10: Prisegtos nuotraukos automatiškai sumažinamos ir sukvadratinamos

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -3,10 +3,24 @@ from django.db import models
 import uuid
 from django.utils import timezone
 from tinymce.models import HTMLField
+from PIL import Image
 
 # Create your models here.
 class CustomUser(AbstractUser):
     photo = models.ImageField(upload_to="profile_pics", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.photo:
+            img = Image.open(self.photo.path)
+            min_side = min(img.width, img.height)
+            left = (img.width - min_side) // 2
+            top = (img.height - min_side) // 2
+            right = left + min_side
+            bottom = top + min_side
+            img = img.crop((left, top, right, bottom))
+            img = img.resize((300, 300), Image.LANCZOS)
+            img.save(self.photo.path)
 
 
 class Author(models.Model):
